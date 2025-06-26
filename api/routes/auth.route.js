9209bc6301d5e892ea63c5e6c0bf0360f30b8c15
pragyakhatwani-iet <22bcs155@ietdavv.edu.in>
2025-06-27 00:59:11 +0530

    changes in auth.route.js

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -2,8 +2,11 @@ import express from "express"
 import {register, login, logout} from "../controllers/auth.controller.js";
 
 const router = express.Router();
+console.log("line before register")
 
 router.post("/register" , register);
+console.log("line after register")
+
 router.post("/login" , login);
 router.post("/logout" , logout);
 
